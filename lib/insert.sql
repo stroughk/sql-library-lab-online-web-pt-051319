436dfb76f67e541c26831cb93ee66823b596f22c
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-03 14:20:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
 INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
@@ -13,3 +14,14 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
+=======
+INSERT INTO series
+(title, author_id, subgenre_id)
+VALUES ("Harry Potter", 1, 1)
+
+
+
+INSERT INTO authors
+(name)
+VALUES ("J.K. Rowling");
+>>>>>>> 425488ad3b9e74a5254d2b0a356d36c059c5b5b5
